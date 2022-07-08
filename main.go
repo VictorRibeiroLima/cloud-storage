b1c3de403c061302a3484ad6fc5e44dfdb8b3033
Victor Ribeiro de Lima <victorlima.12@hotmail.com>
2022-07-08 01:02:39 -0300

    feat: setup project

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	user "github.com/VictorRibeiroLima/cloud-storage/user"
+
+	d "github.com/VictorRibeiroLima/cloud-storage/database"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	if err := godotenv.Load(".env"); err != nil {
+		panic("Error loading env file")
+	}
+
+	setupDb()
+
+	router := gin.Default()
+
+	setRoutes(router)
+
+	router.Run()
+}
+
+func setupDb() {
+	d.InitDb()
+	d.DbConnection.AutoMigrate(&user.User{})
+	println("Database migrated")
+}
+
+func setRoutes(router *gin.Engine) {
+	v1 := router.Group("/v1")
+	{
+		userRoute := v1.Group("/user")
+		{
+			userRoute.GET("/", user.GetUsers)
+			userRoute.POST("/", user.CreateUser)
+		}
+	}
+}
